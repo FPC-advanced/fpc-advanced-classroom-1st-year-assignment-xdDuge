5ce570fb1ec1c456ddf850165ccba9c882911ef0
xDduge <aasishs.bsc23@rvu.edu.in>
2023-10-19 15:51:31 +0530

    committed 10/19/2023, 3:51:31 PM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,16 +1,18 @@
 //06. Write a program to reverse a string.
 
 #include<stdio.h>
+#include<string.h>
 
 void input_string(char *a);
 void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 
-void main(){
-    char a[1000],reva;
+int main(){
+    char a[1000],reva[1000];
     input_string(a);
     str_reverse(a,reva);
     output(a,reva);
+    return 0;
 }
 
 void input_string(char *a){
@@ -19,7 +21,15 @@ void input_string(char *a){
 }
 
 void str_reverse(char *str, char *reva){
-    for(int i=0;i<len(str);i++){
-        
+    int len1 = strlen(str);
+    for(int i=0;i<len1;i++){
+        reva[i] = str[len1-1-i];
     }
+    reva[len1] = '\0';
+
+}
+
+void output(char *a, char *reva){
+        printf("The string before reversing = %s\n",a);
+        printf("The string after reversing is = %s\n",reva);
 }
