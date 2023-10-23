5d9ab52f4711bec2b8ac42b7c46d33aee408fab6
xDduge <aasishs.bsc23@rvu.edu.in>
2023-10-24 02:48:06 +0530

    committed 10/24/2023, 2:48:05 AM

--- set03/problem07.c
+++ set03/problem07.c
@@ -1,11 +1,16 @@
 // Write a program to find the length of a line
 // d=√((x2 – x1)² + (y2 – y1)²)
 
-typedef struct point {
+#include <stdio.h>
+#include <math.h>
+
+typedef struct point
+{
     float x, y;
 } Point;
 
-typedef struct line {
+typedef struct line
+{
     Point p1, p2;
     float distance;
 } Line;
@@ -15,30 +20,43 @@ Line input_line();
 void find_length(Line *l);
 void output(Line l);
 
-int main()
-{
-    Point p1,p2;
-    Line l;
-    p1 = input_point();
-    p2 = input_point();
-    l = input_line();
-
-}
-
 Point input_point()
 {
     Point p;
-    printf("Enter the absicca value of the point : ");
-    scanf("%f",&p.x);
-    printf("Enter the ordinate value of the point : ");
-    scanf("%f",&p.y);
+    printf("Enter the x-coordinate: ");
+    scanf("%f", &p.x);
+    printf("Enter the y-coordinate: ");
+    scanf("%f", &p.y);
     return p;
 }
 
 Line input_line()
 {
     Line l;
-    
+    printf("Enter the coordinates of the first point:\n");
+    l.p1 = input_point();       
+    printf("Enter the coordinates of the second point:\n");
+    l.p2 = input_point();
+    return l;
 }
 
+void find_length(Line *l)
+{
+    float dx,dy;
+    dx = l->p1.x - l->p2.x;
+    dy = l->p1.y - l->p2.y;
+    l->distance = sqrt(dx * dx + dy * dy);
+}
 
+void output(Line l)
+{
+    printf("Length of the line: %f\n", l.distance);
+}
+
+int main()
+{
+    Line line = input_line();
+    find_length(&line);
+    output(line);
+    return 0;
+}
--- set03/problem08.c
+++ set03/problem08.c
@@ -1,2 +1,40 @@
 // Write a program to find the permeter of a polygon
 
+typedef struct point {
+    float x,y;
+} Point;
+
+
+typedef struct polygon {
+    int sides;
+    Point p[100];
+    float perimeter;
+} Polygon;
+
+int input_n();
+Point input_point(char *promt_msg);
+int input_polygon(Polygon *p);
+float find_distance(Point a, Point b);
+void find_perimeter(Polygon* p);
+void output(Polygon p);
+
+int main()
+{
+    Polygon poly;
+    int n;
+    n = input_n();
+
+}
+
+int input_n()
+{
+    int n;
+    printf("Enter the sumber of sides: \n");
+    scanf("%d",&n);
+    return n;
+}
+
+Point input_point(char *prompt_msg)
+{
+    
+}
