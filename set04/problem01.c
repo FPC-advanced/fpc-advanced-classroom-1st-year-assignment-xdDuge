f5c49b2ebf56c6d14d525a4607159993697361c6
xDduge <aasishs.bsc23@rvu.edu.in>
2023-10-31 12:08:38 +0530

    committed 10/31/2023, 12:08:37 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -35,5 +35,5 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
 {
-    printf()
+    printf("The ")
 }
