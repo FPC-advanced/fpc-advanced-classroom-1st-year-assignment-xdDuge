ab9743de390679c51287a999830012bb8d1137b2
xDduge <aasishs.bsc23@rvu.edu.in>
2023-11-01 16:14:59 +0530

    committed 11/1/2023, 4:14:57 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -35,6 +35,9 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
 {
-    for(int i = 1;i<=res_num && i<=res_den;)
+    for(int i = 1;i<=res_num && i<=res_den;i++)
+    {
+        if(res_num%i ==0 && res_den%)
+    }
     printf("The sum of two fractions %d/%d and %d/%d is = %d/%d",num1,den1,num2,den2,res_num,res_den);
 }
