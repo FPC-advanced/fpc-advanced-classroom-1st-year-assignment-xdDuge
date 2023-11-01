f9ee2dca989a99880fbde10064ef61d25aff2ad5
xDduge <aasishs.bsc23@rvu.edu.in>
2023-11-01 16:13:29 +0530

    committed 11/1/2023, 4:13:27 PM

--- set04/problem01.c
+++ set04/problem01.c
@@ -31,6 +31,7 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
 {
     *res_num = (num1*den2) + (num2*den1);
     *res_den = den1*den2;//simple criss cross method
+    int hcf = 
 }
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
