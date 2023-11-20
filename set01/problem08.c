04967652ec5bee8f68ca987f200d5ddff4559d0c
xDduge <aasishs.bsc23@rvu.edu.in>
2023-11-20 10:49:28 +0530

    committed 11/20/2023, 10:49:27 AM

--- set01/problem08.c
+++ set01/problem08.c
@@ -44,7 +44,7 @@ void output(int n, int a[n], int sum)
     for(int i=0;i<n;i++)
     {
         printf("%d ",a[i]);
-        if(i!=n)
+        if(i!=n-1)
         {
             printf("+");
         }
